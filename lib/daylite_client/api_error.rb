176f6b5f1b8c603ffa1ee06a7dd1af5ce3bb982b
Linch <andrei95-1995@mail.ru>
2017-07-28 11:51:14 +0300

    change DayliteAPIError class to APIError | move from Middleware module
    to DayliteClient

---
+++ lib/daylite_client/api_error.rb
@@ -0,0 +1,14 @@
+module DayliteClient
+  class APIError < StandardError
+
+    attr_reader :status, :message, :description
+
+    def initialize(status = nil, description = "Unknown error")
+      @status      = status
+      @description = description
+      @message     = "#{status.to_s + ': ' if status.present?}#{description}"
+      super(@message)
+    end
+
+  end
+end
