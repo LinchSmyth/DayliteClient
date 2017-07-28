cf66bc0d7be590781e8046c1b1238efa4c818ddb
Linch <andrei95-1995@mail.ru>
2017-07-28 11:57:13 +0300

    use renamed error class | add comment for complicated code

--- lib/daylite_client/middleware/parser.rb
+++ lib/daylite_client/middleware/parser.rb
@@ -6,7 +6,7 @@ module DayliteClient
         # errors processing since shitty daylite api
         # returns html instead of json in a case when api_key is outdated
         if unacceptable_codes.has_key?(env[:status])
-          raise DayliteAPIError.new(env[:status], unacceptable_codes[env[:status]])
+          raise APIError.new(env[:status], unacceptable_codes[env[:status]])
         else
           original_json = MultiJson.load(env[:body])
 
@@ -14,6 +14,11 @@ module DayliteClient
           # so we need to handle it and process only objects
           if original_json.is_a?(Array)
             json = {}
+
+            # this complicated code generates different json[:data] values
+            # it can be `[]` or `{ collection: [] }`
+            # because Her gem throws an error when Contact.all is a hash with :collection key
+            # but without this it throws an error for every resource, that's just array of strings (like Labels)
             original_json.each do |item|
               if item.is_a?(Hash)
                 (json[:data] ||= []).push(item.deep_transform_keys{ |k| string_to_snakecase(k.to_s).to_sym })
