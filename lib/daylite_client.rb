c9ee0d4a5d10477290434ccfeb415a3fc6e746f0
Linch <andrei95-1995@mail.ru>
2017-07-28 11:56:13 +0300

    change double quotes to single quotes

--- lib/daylite_client.rb
+++ lib/daylite_client.rb
@@ -6,18 +6,19 @@ require 'her'
 require 'pry'
 
 # Basic classes
-require "daylite_client/version"
-require "daylite_client/client"
-require "daylite_client/middleware"
-require "daylite_client/resource"
+require 'daylite_client/version'
+require 'daylite_client/client'
+require 'daylite_client/middleware'
+require 'daylite_client/api_error'
+require 'daylite_client/resource'
 
 # Daylite api models
-require "daylite_client/api_models/user"
-require "daylite_client/api_models/contact"
-require "daylite_client/api_models/company"
-require "daylite_client/api_models/label"
-require "daylite_client/api_models/note"
-require "daylite_client/api_models/opportunity"
+require 'daylite_client/api_models/user'
+require 'daylite_client/api_models/contact'
+require 'daylite_client/api_models/company'
+require 'daylite_client/api_models/label'
+require 'daylite_client/api_models/note'
+require 'daylite_client/api_models/opportunity'
 
 module DayliteClient
   def self.root
--- lib/daylite_client/middleware.rb
+++ lib/daylite_client/middleware.rb
@@ -1,6 +1,5 @@
-require "daylite_client/middleware/daylite_api_error"
-require "daylite_client/middleware/authentication"
-require "daylite_client/middleware/parser"
+require 'daylite_client/middleware/authentication'
+require 'daylite_client/middleware/parser'
 
 module DayliteClient
   module Middleware
